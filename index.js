660098e883d3cfc061157fcb277be73208e4cb2f
fov42550564 <42550564@qq.com>
2017-03-30 14:05:18 +0800

    eslint code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 } = ReactNative;
 const _ = require('lodash');
 const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource.js');
-resolveAssetSource.setCustomSourceTransformer((resolver)=>{
+resolveAssetSource.setCustomSourceTransformer((resolver) => {
     if (Platform.OS === 'android' && !resolver.serverUrl && !resolver.bundlePath && resolver.asset.type === 'html') {
         resolver.bundlePath = '/android_res/';
     }
@@ -19,61 +19,61 @@ resolveAssetSource.setCustomSourceTransformer((resolver)=>{
 const source = require('./remobile-echarts.html');
 
 module.exports = React.createClass({
-    getDefaultProps() {
+    getDefaultProps () {
         return {
             width: Dimensions.get('window').width,
             height: 400,
         };
     },
-    componentWillReceiveProps(nextProps) {
-        if(!this.loading && !_.isEqual(nextProps.option, this.props.option)) {
+    componentWillReceiveProps (nextProps) {
+        if (!this.loading && !_.isEqual(nextProps.option, this.props.option)) {
             this.refs.chart.reload();
         }
     },
-    getOption(obj) {
+    getOption (obj) {
         const escape = `-=remobile=-`;
-        return JSON.stringify(obj, function(key, val) {
+        return JSON.stringify(obj, function (key, val) {
             if (typeof val === 'function') {
                 return `${escape}${val}${escape}`;
             }
             return val;
         }).replace(`"${escape}`, '').replace(`${escape}"`, '').replace(/\\n/g, '');
     },
-    getInjectedJavaScript() {
-        const {width, height, option} = this.props;
-        const params = {...option, animation: false};
+    getInjectedJavaScript () {
+        const { width, height, option } = this.props;
+        const params = { ...option, animation: false };
         return `
         document.body.style.height = "${height}px";
         document.body.style.width = "${width}px";
         echarts.init(document.body).setOption(${this.getOption(params)})
         `;
     },
-    reload() {
+    reload () {
         this.refs.chart.reload();
     },
-    onLoadStart() {
+    onLoadStart () {
         this.loading = true;
     },
-    onLoadEnd() {
+    onLoadEnd () {
         this.loading = false;
     },
-    render() {
-        const {width, height} = this.props;
+    render () {
+        const { width, height } = this.props;
         const script = this.getInjectedJavaScript();
         return (
-            <View style={{width, height}}>
+            <View style={{ width, height }}>
                 <WebView
-                    ref="chart"
+                    ref='chart'
                     onLoadStart={this.onLoadStart}
                     onLoadEnd={this.onLoadEnd}
                     scrollEnabled={false}
-                    scalesPageToFit={true}
-                    startInLoadingState={true}
+                    scalesPageToFit
+                    startInLoadingState
                     injectedJavaScript={script}
-                    style={{width, height}}
+                    style={{ width, height }}
                     source={source}
                     />
-                <View style={{width, height, position:'absolute', top:0, left:0, backgroundColor:'transparent'}} />
+                <View style={{ width, height, position:'absolute', top:0, left:0, backgroundColor:'transparent' }} />
             </View>
         );
     },
